a78c1dfc9088b8fc3e5ccc292e897efbaa551660
Sergey <s.zharkov78@gmail.com>
2023-01-24 20:22:19 +0100

    added book a table

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express');
 
+import tableRouter from './router/tables.js';
+
+// ROUTES
+app.use('/table', tableRouter);
 
 const app = express();
 const port = process.env.PORT || 5000;
-app.get('/api', (req, res) => {
+app
+  .get('/api', (req, res) => {
     res.send('Hello World');
-})
-.listen(port, () => console.log(`Listening on port http://localhost:${port}`));
-
-
+  })
+  .listen(port, () =>
+    console.log(`Listening on port http://localhost:${port}`)
+  );
