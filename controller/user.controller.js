03d288a14e91dc032ef3aef02871ae2dddd2c9a4
Abdulla Alwaki <Abdullaalwaki@icloud.com>
2023-02-27 14:39:38 +0100

    test

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,12 +1,11 @@
-import User from '../models/user.models.js';
-import jwt from 'jsonwebtoken';
-import crypto from 'crypto';
-import bcrypt from 'bcrypt';
+import User from "../models/user.models.js";
+import jwt from "jsonwebtoken";
+import crypto from "crypto";
+import bcrypt from "bcrypt";
 
-
-import { Token } from '../models/token.models.js';
-import {transporter} from '../midlleware/auth.middleware.js';
-import { verify_message } from '../helper/template.js';
+import { Token } from "../models/token.models.js";
+import { transporter } from "../midlleware/auth.middleware.js";
+import { verify_message } from "../helper/template.js";
 
 // get all Users
 export async function getUsers(req, res, next) {
@@ -22,7 +21,7 @@ export async function getUsers(req, res, next) {
 export async function getUser(req, res, next) {
   try {
     const user = await User.findById(req.params.id);
-    if (!user) return res.status(404).json({ message: 'User not found!' });
+    if (!user) return res.status(404).json({ message: "User not found!" });
     res.status(200).json(user);
   } catch (error) {
     next(error);
@@ -34,18 +33,24 @@ export async function updateUser(req, res, next) {
   try {
     let updatedUser;
 
-    if(req.body.password){
+    if (req.body.password) {
       const hashedPassword = await bcrypt.hash(req.body.password, 10);
       updatedUser = await User.findByIdAndUpdate(
         req.params.id,
         { ...req.body, password: hashedPassword },
         { new: true }
       );
-    }else{
-      updateUser = await User.findByIdAndUpdate( req.params.id, { ...req.body }, { new: true });
+    } else {
+      updateUser = await User.findByIdAndUpdate(
+        req.params.id,
+        { ...req.body },
+        { new: true }
+      );
     }
 
-    res.status(200).json({message: 'User updated successfully!',data: {updatedUser}});
+    res
+      .status(200)
+      .json({ message: "User updated successfully!", data: { updatedUser } });
   } catch (error) {
     next(error);
   }
@@ -55,8 +60,8 @@ export async function updateUser(req, res, next) {
 export async function deleteUser(req, res, next) {
   try {
     const user = await User.findByIdAndDelete(req.params.id);
-    if (!user) return res.status(404).json({ message: 'User not found!' });
-    res.status(200).json({ message: 'User deleted successfully!' });
+    if (!user) return res.status(404).json({ message: "User not found!" });
+    res.status(200).json({ message: "User deleted successfully!" });
   } catch (e) {
     next(e);
   }
@@ -66,18 +71,20 @@ export async function deleteUser(req, res, next) {
 
 export const signUp = async (req, res, next) => {
   try {
-    const {firstName,lastName, email, password, confirm } = req.body;
+    const { firstName, lastName, email, password, confirm } = req.body;
 
     if (!firstName || !lastName || !email || !password || !confirm) {
       return res
         .status(400)
-        .json({ message: `Please fill out the required (*) fields for sign-up!` });
+        .json({
+          message: `Please fill out the required (*) fields for sign-up!`,
+        });
     }
 
     const foundUser = await User.findOne({ email });
 
     if (foundUser)
-      return res.status(203).json({ message: 'you already in our app' });
+      return res.status(203).json({ message: "you already in our app" });
 
     const user = await User.create({
       firstName,
@@ -88,10 +95,11 @@ export const signUp = async (req, res, next) => {
     });
 
     let token;
-    if(user){
-      token = jwt.sign({ id: user._id }, process.env.SECRET, { expiresIn: '1d' });
+    if (user) {
+      token = jwt.sign({ id: user._id }, process.env.SECRET, {
+        expiresIn: "1d",
+      });
     }
-
 
     const verification_token = await Token.create({
       id: user._id,
@@ -102,27 +110,28 @@ export const signUp = async (req, res, next) => {
     const verifyLink = `${process.env.BASE_URL}/api/verify/${user._id}/${verification_token.token}`;
 
     //send email to new user
-    
-    await transporter.sendMail({
-      from: `MERN ${process.env.EMAIL}`,
-      to: email,
-      subject: "Welcome to our App!",
-      html: verify_message(verifyLink, firstName + " "+ lastName),
-    }).catch((err)=>{
-      console.log(err);
-    }
-    );
+
+    await transporter
+      .sendMail({
+        from: `MERN ${process.env.EMAIL}`,
+        to: email,
+        subject: "Welcome to our App!",
+        html: verify_message(verifyLink, firstName + " " + lastName),
+      })
+      .catch((err) => {
+        console.log(err);
+      });
 
     res
-    .status(201)
-      .cookie('access_token', token, { httpOnly: true, expires : new Date(Date.now() + 1000 * 60 * 60 * 24 * 7) })
-      .json({ message: 'you signUp successfully',
-      token: token,
-      user: user });
+      .status(201)
+      .cookie("access_token", token, {
+        httpOnly: true,
+        expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 7),
+      })
+      .json({ message: "you signUp successfully", token: token, user: user });
   } catch (error) {
-    console.log(error)
+    console.log(error);
     next(error);
-
   }
 };
 
@@ -133,31 +142,37 @@ export const signIn = async (req, res, next) => {
     const { email, password } = req.body;
 
     if (!email || !password) {
-      return res.status(401).send({error: "email or password invalid"})
+      return res.status(401).send({ error: "email or password invalid" });
     }
 
-    const user = await User.findOne({ email }).select('+password');
+    const user = await User.findOne({ email }).select("+password");
 
     if (!user) {
-      return res.status(404).send({error: "This User doesn't exist"})
+      return res.status(404).send({ error: "This User doesn't exist" });
     }
 
-    if (user && await user.checkPassword(password, user.password)) {
-      const token = jwt.sign({ id: user._id }, process.env.SECRET, { expiresIn: '1d' });
+    if (user && (await user.checkPassword(password, user.password))) {
+      const token = jwt.sign({ id: user._id }, process.env.SECRET, {
+        expiresIn: "1d",
+      });
       res
-        .cookie('access_token', token, {
+        .cookie("access_token", token, {
           httpOnly: true,
-          maxAge: 3600000 * 5,
           // secure : process.env.NODE_ENV == "dev"? false : true,
-          sameSite: 'none',
+          // sameSite: 'none',
           expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 7),
-        }).send ({
-          message: 'you login successfully',
+        })
+        .send({
+          message: "you login successfully",
           token: token,
           user: user,
         });
     } else {
-    res.status(401).json({ error: "Username or Password is not valid or u r not in our app" });
+      res
+        .status(401)
+        .json({
+          error: "Username or Password is not valid or u r not in our app",
+        });
     }
   } catch (error) {
     next(error);
@@ -168,36 +183,35 @@ export const signIn = async (req, res, next) => {
 
 export const logout = async (req, res, next) => {
   try {
-    res.clearCookie('access_token');
-    res.status(200).json({ message: 'you logout successfully' });
+    res.clearCookie("access_token");
+    res.status(200).json({ message: "you logout successfully" });
   } catch (error) {
     next(error);
   }
-}
-
+};
 
 //--------verify---------
 export const verify = async (req, res, next) => {
   try {
     const { id, token } = req.params;
-    
+
     //check the id and token in the database
     const user = await User.findById(id);
-    if (!user) return res.status(404).json({ message: 'User not found!' });
+    if (!user) return res.status(404).json({ message: "User not found!" });
 
     const tokenInDB = await Token.findOne({ id: id, token: token });
-    if (!tokenInDB) return res.status(404).json({ message: 'Token not found!' });
+    if (!tokenInDB)
+      return res.status(404).json({ message: "Token not found!" });
 
     //update the user's status to verified
     await User.findByIdAndUpdate(id, { isVerified: true }, { new: true });
     await Token.findByIdAndDelete(tokenInDB._id);
 
-    res.status(200).json({ message: 'User verified successfully!' });
-
+    res.status(200).json({ message: "User verified successfully!" });
   } catch (error) {
     next(error);
   }
-}
+};
 
 //-----------------forgot password-----------------
 
@@ -207,7 +221,7 @@ export const forgotPassword = async (req, res, next) => {
 
     //check if the email is in the database
     const check = await User.findOne({ email });
-    if (!check) return res.status(404).json({ message: 'Email not found!' });
+    if (!check) return res.status(404).json({ message: "Email not found!" });
 
     //create a reset token
     const reset_token = await Token.create({
@@ -226,10 +240,8 @@ export const forgotPassword = async (req, res, next) => {
       html: reset_message(resetLink, check.name),
     });
 
-    res.status(200).json({ message: 'Email sent successfully!' });
-
+    res.status(200).json({ message: "Email sent successfully!" });
   } catch (error) {
-    next(error); 
+    next(error);
   }
-    
-  }
+};
--- server.js
+++ server.js
@@ -1,29 +1,34 @@
-import express, { json, urlencoded } from 'express';
-import { db } from './helper/db.js';
-import cors from 'cors';
-import UserRouter from './router/user.router.js';
-import AddDishesRouter from './router/addDishes.router.js';
-import ContactRouter from './router/contact.router.js';
-import tableRouter from './router/table.router.js';
+import express, { json, urlencoded } from "express";
+import { db } from "./helper/db.js";
+import cors from "cors";
+import UserRouter from "./router/user.router.js";
+import AddDishesRouter from "./router/addDishes.router.js";
+import ContactRouter from "./router/contact.router.js";
+import tableRouter from "./router/table.router.js";
+import cookieParser from "cookie-parser";
+
 // import morgan from "morgan";
 
 db();
 const app = express();
+app.use(cookieParser());
 app.use(json());
 app.use(urlencoded({ extended: true }));
 
 const corsOption = {
-  origin: ['http://localhost:3000', 'https://mern-restaurant.onrender.com'],
+  origin: ["http://localhost:3000", "https://mern-restaurant.onrender.com"],
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  methods: ["GET", "POST", "PUT", "DELETE"],
 };
 
+
 // morgan("tiny");
-app.use(cors(corsOption));
-app.use('/api', UserRouter);
-app.use('/api', AddDishesRouter);
-app.use('/api', ContactRouter);
-app.use('/api', tableRouter);
+app.use(cors(/* corsOption */));
+
+app.use("/api", UserRouter);
+app.use("/api", AddDishesRouter);
+app.use("/api", ContactRouter);
+app.use("/api", tableRouter);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () =>
